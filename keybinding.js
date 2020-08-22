37b435c036036e15a140e207015c08bd5e3bbea8
nitti87 <44554259+nitti87@users.noreply.github.com>
2020-08-22 23:28:18 +0200

    Update keybinding.js

--- keybinding.js
+++ keybinding.js
@@ -1,8 +1,7 @@
 class Key {
-  constructor(option = {}) {
+  constructor() {
     this.key = {
-      map: require('../json/keyboard_map.json'), 
-      bind: require('../json/keyboard_bind.json'), // for personal use
+      map: require('@json/keyboard_map.json'), 
       onElement: undefined
     }
   }
@@ -18,7 +17,8 @@ class Key {
     const option = {
       to: opt.to && ((/\b(keyup|keydown)\b/).test(opt.to.toLowerCase())) ? opt.to.toLowerCase() : 'keyup',  // maybe add keypress as well (With ASCII-code)
       do: typeof opt.do === 'object' && opt.do.if !== undefined ? opt.do.ret : (typeof opt.do === 'function' ? opt.do : false),
-      do_if: typeof opt.do === 'object' && opt.do.if !== undefined ? opt.do.if.toLowerCase() || false : false
+      do_if: typeof opt.do === 'object' && opt.do.if !== undefined ? opt.do.if.toLowerCase() || false : false,
+      addKeyCode: typeof opt.do === 'object' && opt.do.addKeyCode ? opt.do.addKeyCode : false
     }
 
     const do_withoutIf = (element, i) => {
@@ -28,8 +28,8 @@ class Key {
     }
 
     const do_withIf = async (element, indexOfElement) => {
-     const splitIf = option.do_if.split(/(?:or)|[|]/).map(arr => arr.trim().split(/[+&]/))
-     let event = []
+      const splitIf = option.do_if.split(/(?:or)|[|]/).map(arr => arr.trim().split(/[+&]/))
+      let event = []
 
       element.addEventListener(option.to, (evt) => {
         event.push(parseInt(evt.which || evt.keyCode))
@@ -37,7 +37,7 @@ class Key {
         
          splitIf.forEach((arr, index) => {
           arr.forEach((item, i) => {
-            if(isNaN(item)) {
+            if(!option.addKeyCode) {
               for (let [key, val] of Object.entries(this.key.map.code)) {
                 if(item === key) {
                   splitIf[index][i] = val
@@ -54,6 +54,23 @@ class Key {
     }
     
     this.key.onElement === undefined ? (option.do_if ? do_withIf(document) : do_withoutIf(document)) : this.key.onElement.forEach((el) => { option.do_if ? do_withIf(el) : do_withoutIf(el) })
+    return this
+  }
+
+  preventTyping(options = 'xss') {    
+    const arrOfXSS = [
+      `<`, `>`, `'`, `"`, `/`, `(`, `&`, `#`, `;`, `=`
+    ]; 
+    
+    const option = {
+      what: options === 'xss' ? arrOfXSS : options
+    }
+
+    this.key.onElement.forEach(el => {
+      el.addEventListener('keydown', e => {
+        option.what.includes(e.key) ? e.preventDefault() : null
+      })
+    })
 
     return this
   }
